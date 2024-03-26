1232a60130698d4b1bf7cf4efc3597782cde018b
Shital Mane <shitalmane401@gmail.com>
2024-03-26 16:51:26 +0530

    exam coding test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import Header from './components/header/Header';
-import Main from './components/main/Main';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Carousel from './components/carousel/Carousel';
+import Exam from './components/exam/Exam';
 
 export default function App() {
   return (
-    <div>
-      {/* <Header /> */}
-      <Navbar />
-      {/* <Carousel/> */}
-      <Main />
-      <Footer />
-    </div>
+    <div className="main__wrap">
+    <main className="container">
+      <div>
+        <Exam/>
+      </div>
+    </main>
+  </div>
   )
 }
