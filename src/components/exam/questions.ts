553ee4c2ae64c1a376d0d8c04d76ce641ca4215b
Shital Mane <shitalmane401@gmail.com>
2024-03-26 17:41:03 +0530

    Updates

--- src/components/exam/questions.ts
+++
@@ -1,9 +0,0 @@
-const questions = [
-    { id: 1, text: 'Question 1: Can you code in Ruby?' },
-    { id: 2, text: 'Question 2: Can you code in JavaScript?' },
-    { id: 3, text: 'Question 3: Can you code in Swift?' },
-    { id: 4, text: 'Question 4: Can you code in Java?' },
-    { id: 5, text: 'Question 5: Can you code in C#?' },
-  ];
-
-  export default questions;
--- src/components/footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div style={{height:'50px',backgroundColor:'black',color:'#fff'}}>Footer</div>
-  )
-}
